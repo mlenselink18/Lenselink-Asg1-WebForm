42d1c44ea2da7bbf65ab841f445c37f58426111f
mlenselink18 <mlenselink18@gmail.com>
2022-03-20 15:15:10 -0600

    Add project files.

---
+++ Lenselink-Asg1-WebForm/Default.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lenselink_Asg1_WebForm.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Lenselink Asg1 Web Form Calculator</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <h1>
+                Michael Lenselink Asg1 Web Form
+            </h1>
+        </div>
+        <asp:Panel ID="PanelCalculator" runat="server" Height="229px">
+            *non-numeric input is converted to zero*<br />
+            <br />
+            <asp:TextBox ID="TextBoxInput1" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;<asp:Label ID="LabelOperator" runat="server" Text="Label" Visible="False"></asp:Label>
+            &nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBoxInput2" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp; =&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TextBoxOutput" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelError" runat="server" Text="Label" Visible="False"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="ButtonAdd" runat="server" OnClick="ButtonAdd_Click" Text="+" />
+            &nbsp;<asp:Button ID="ButtonSubtract" runat="server" OnClick="ButtonSubtract_Click" Text="-" />
+            &nbsp;<asp:Button ID="ButtonMultiply" runat="server" OnClick="ButtonMultiply_Click" Text="*" Width="22px" />
+            &nbsp;<asp:Button ID="ButtonDivide" runat="server" OnClick="ButtonDivide_Click" Text="/" />
+            <br />
+            <br />
+            <asp:Button ID="ButtonOne" runat="server" OnClick="NumberButton_Click" Text="1" />
+            <asp:Button ID="ButtonTwo" runat="server" OnClick="NumberButton_Click" Text="2" />
+            <asp:Button ID="ButtonThree" runat="server" OnClick="NumberButton_Click" Text="3" />
+            <br />
+            <asp:Button ID="ButtonFour" runat="server" OnClick="NumberButton_Click" Text="4" />
+            <asp:Button ID="ButtonFive" runat="server" OnClick="NumberButton_Click" Text="5" />
+            <asp:Button ID="ButtonSix" runat="server" OnClick="NumberButton_Click" Text="6" />
+            <br />
+            <asp:Button ID="ButtonSeven" runat="server" OnClick="NumberButton_Click" Text="7" />
+            <asp:Button ID="ButtonEight" runat="server" OnClick="NumberButton_Click" Text="8" />
+            <asp:Button ID="ButtonNine" runat="server" OnClick="NumberButton_Click" Text="9" />
+            <br />
+            &nbsp;<asp:Button ID="ButtonNegative" runat="server" OnClick="NumberButton_Click" Text="-" />
+            <asp:Button ID="ButtonZero" runat="server" OnClick="NumberButton_Click" Text="0" />
+            <asp:Button ID="ButtonDot" runat="server" OnClick="NumberButton_Click" Text="." />
+            <br />
+            <br />
+            <asp:Button ID="ButtonSolve" runat="server" OnClick="ButtonSolve_Click" Text="=" />
+&nbsp;
+            <asp:Button ID="ButtonReset" runat="server" OnClick="ButtonReset_Click" style="height: 29px" Text="CE" />
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ Lenselink-Asg1-WebForm/Default.aspx.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lenselink_Asg1_WebForm
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public static TextBox selectedInput;
+
+        public static List<Button> InputButtons = new List<Button>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                LoadButtonsToList();
+                resetAll();
+                selectedInput = TextBoxInput1;
+            }
+        }
+
+        public void LoadButtonsToList()
+        {
+            InputButtons.Add(ButtonOne);
+            InputButtons.Add(ButtonTwo);
+            InputButtons.Add(ButtonThree);
+            InputButtons.Add(ButtonFour);
+            InputButtons.Add(ButtonFive);
+            InputButtons.Add(ButtonSix);
+            InputButtons.Add(ButtonSeven);
+            InputButtons.Add(ButtonEight);
+            InputButtons.Add(ButtonNine);
+            InputButtons.Add(ButtonZero);
+            InputButtons.Add(ButtonDot);
+            InputButtons.Add(ButtonNegative);
+        }
+
+        public void toggleEnabled()
+        {
+            ButtonAdd.Enabled = false;
+            ButtonSubtract.Enabled = false;
+            ButtonMultiply.Enabled = false;
+            ButtonDivide.Enabled = false;
+            TextBoxInput2.Enabled = true;
+            TextBoxInput1.Enabled = false;
+            selectedInput = TextBoxInput2;
+        }
+
+        public decimal safeDecimalCast(string input)
+        {
+            decimal result = 0;
+
+            bool success = decimal.TryParse(input, out result);
+
+            if(success)
+            {
+                result = decimal.Parse(input);
+            }
+            else
+            {
+                LabelError.Text = "Please enter valid numbers";
+                LabelError.ForeColor = Color.Red;
+                LabelError.Visible = true;
+            }
+
+            return result;
+        }
+
+        public decimal[] getInputs()
+        {
+            decimal[] inputs = { safeDecimalCast(TextBoxInput1.Text), safeDecimalCast(TextBoxInput2.Text)};
+
+            if (inputs[0] == 0)
+                TextBoxInput1.Text = "0";
+            if (inputs[1] == 0)
+                TextBoxInput2.Text = "0";
+
+            return inputs;
+        }
+
+        protected void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            LabelOperator.Text = "+";
+            LabelOperator.Visible = true;
+            toggleEnabled();
+        }
+
+        protected void ButtonSubtract_Click(object sender, EventArgs e)
+        {
+            LabelOperator.Text = "-";
+            LabelOperator.Visible = true;
+            toggleEnabled();
+        }
+        protected void ButtonMultiply_Click(object sender, EventArgs e)
+        {
+            LabelOperator.Text = "*";
+            LabelOperator.Visible = true;
+            toggleEnabled();
+        }
+
+        protected void ButtonDivide_Click(object sender, EventArgs e)
+        {
+            LabelOperator.Text = "/";
+            LabelOperator.Visible = true;
+            toggleEnabled();
+        }
+
+        public void ClearError()
+        {
+            LabelError.Visible = false;
+        }
+
+        protected void NumberButton_Click(object sender, EventArgs e)
+        {
+            string test1 = TextBoxInput1.Text;
+            string test2 = TextBoxInput2.Text;
+            var button = sender as Button;
+            var args = button.Text;
+            string newText = "";
+            if (selectedInput.ClientID == "TextBoxInput2")
+            {
+                newText = TextBoxInput2.Text + args.ToString();
+                TextBoxInput2.Text = newText;
+            }
+            else
+            {
+                newText = test1 + args.ToString();
+                TextBoxInput1.Text = newText;
+            }
+        }
+
+        protected void ButtonSolve_Click(object sender, EventArgs e)
+        {
+            toggleButtons(false);
+            SolveMath();
+        }
+
+        protected void ButtonReset_Click(object sender, EventArgs e)
+        {
+            resetAll();
+        }
+
+        public void resetAll()
+        {
+            ClearError();
+            toggleButtons(true);
+            TextBoxInput1.Text = "";
+            TextBoxInput2.Text = "";
+            LabelOperator.Visible = false;
+            LabelOperator.Text = "";
+            TextBoxOutput.Text = "";
+            selectedInput = TextBoxInput1;
+            ButtonAdd.Enabled = true;
+            ButtonSubtract.Enabled = true;
+            ButtonMultiply.Enabled = true;
+            ButtonDivide.Enabled = true;
+            TextBoxInput2.Enabled = false;
+            TextBoxInput1.Enabled = true;
+        }
+
+        public void SolveMath()
+        {
+            ClearError();
+            var inputs = getInputs();
+            var op = LabelOperator.Text;
+            string result = "";
+
+            switch (op)
+            {
+                case ("+"):
+                    result = (inputs[0] + inputs[1]).ToString("N4");
+                    break;
+                case ("-"):
+                    result = (inputs[0] - inputs[1]).ToString("N4");
+                    break;
+                case ("*"):
+                    result = (inputs[0] * inputs[1]).ToString("N4");
+                    break;
+                case ("/"):
+                    if (inputs[1] != 0)
+                        result = (inputs[0] / inputs[1]).ToString("N4");
+                    else
+                    {
+                        LabelError.Text = "Unable to divide by zero";
+                        LabelError.ForeColor = Color.Red;
+                        LabelError.Visible = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            TextBoxOutput.Text = result;
+        }
+
+        public void toggleButtons(bool state)
+        {
+            foreach(Button item in InputButtons)
+            {
+                var button = Page.FindControl(item.ClientID) as Button;
+                button.Enabled = state;
+            }
+        }
+    }
+}
